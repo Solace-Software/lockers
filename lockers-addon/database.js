4ca5b70b09f5f0ebfc4b0b54a662ca5feb44c1cf
Michael Hanna <michaelhanna@solacecontrols.net>
2025-07-23 15:55:12 +0300

    Add debug logging for DB environment variables to troubleshoot add-on config

--- lockers-addon/database.js
+++ lockers-addon/database.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql2/promise');
 
+// Debug: Print DB environment variables
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 class Database {
   constructor() {
     this.pool = mysql.createPool({
