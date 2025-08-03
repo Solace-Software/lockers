55bf8312bad0df18f6679fb929809274497440f8
Michael Hanna <michaelhanna@solacecontrols.net>
2025-08-03 09:49:31 +0300

    clean addon repository - remove duplicate directories and unnecessary files

--- gym_lockers/client/src/components/locker/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LockerCard } from './LockerCard';
-export { default as LockerFilters } from './LockerFilters';
-export { default as LockerStats } from './LockerStats';
--- gym_lockers/client/src/components/ui/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Select } from './Select';
-export { default as Card } from './Card';
-export { default as StatusBadge } from './StatusBadge';
-export { default as Table } from './Table';
-export { default as Icon } from './Icon';
